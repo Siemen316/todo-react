66d0c3b4fab1836fc6c922dee1a3eb545d58c81f
siemen <siemensubbaiah1@gmail.com>
2020-09-29 17:19:08 +0530

    test!

---
+++ precache-manifest.cc11c67dc413e941eaf36be7ccbbfd6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23951c763df23e2cca04",
+    "url": "/static/css/main.bbe46e2c.chunk.css"
+  },
+  {
+    "revision": "23951c763df23e2cca04",
+    "url": "/static/js/main.23951c76.chunk.js"
+  },
+  {
+    "revision": "dacaedd80d7fe32e769c",
+    "url": "/static/js/1.dacaedd8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cc7e58126d73ad7f971ddf5e1b599ae6",
+    "url": "/index.html"
+  }
+];
